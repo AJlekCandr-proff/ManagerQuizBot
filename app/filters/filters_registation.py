2ef64cc58ff20c9f41b343772f6638e29a98d003
AJlekCandr_proff <svznikov@yandex.ru>
2024-11-27 22:06:31 +0300

    feat: Addition handler for entering name pupil.

--- app/filters/filters_registation.py
+++ app/filters/filters_registation.py
@@ -1,3 +1,5 @@
+from re import fullmatch
+
 from aiogram.filters import Filter
 from aiogram.types import Message
 
@@ -6,3 +8,10 @@ class RegistrationFilter(Filter):
     async def __call__(self, message: Message) -> Message | None:
         if message.text == '📝 Регистрация':
             return message
+
+
+class EnterName(Filter):
+    async def __call__(self, message: Message) -> Message | None:
+        if message.text:
+            if fullmatch(r'^[А-Яа-я]+\s+[А-Яа-я]+$', message.text):
+                return message
--- app/handlers/handler_registration.py
+++ app/handlers/handler_registration.py
@@ -1,13 +1,24 @@
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 
 from ..configuration.settings import views
-from ..filters.filters_registation import RegistrationFilter
+from ..filters.filters_registation import RegistrationFilter, EnterName
+from ..utils.states_form import States
 
 
 router = Router(name=__name__)
 
 
 @router.message(RegistrationFilter())
-async def handler_registration(message: Message) -> None:
+async def handler_registration(message: Message, state: FSMContext) -> None:
     await message.answer(text=f"{message.from_user.first_name}, {views.get('registration_view')}")
+
+    await message.answer(text=views.get('set_name'))
+
+    await state.set_state(States.get_name)
+
+
+@router.message(States.get_name, EnterName())
+async def handler_get_name(message: Message) -> None:
+    await message.answer(text=views.get('choice_category'))
